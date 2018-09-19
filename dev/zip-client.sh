a8417ee870200ad7d7cb3d9ae615c8fd065f7c00
Josh Beckwith <josh.beckwith@gmail.com>
2018-09-19 11:28:49 -0700

    zippewr

---
+++ dev/zip-client.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# creates a clean zip of the client folder, ready to upload to twitch
+
+# TODO
+
+# cp -R ../server/client/ /tmp/client
+# rm -R /tmp/client/css
+# rm -R /tmp/client/js
+
+# cd /tmp/
+# zip -r -X client.zip client/
+# mv client.zip ~/Desktop/client.zip
